e7a24b98dbbbadbe40d1b18df2b013486373ca9b
Tanmoy741127 <ts741127@gmail.com>
2021-05-13 15:27:48 +0530

    update

--- new.py
+++ new.py
@@ -55,8 +55,8 @@ endpoint = 'https://excise.wb.gov.in/CHMS/Public/Page/CHMS_Public_Hospital_Bed_A
 
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--window-size=1920x1080")
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--window-size=1920x1080")
 
 
 #Functions
@@ -69,11 +69,20 @@ def init_scrape(chrome_options,endpoint,PATH_TO_DRIVER=r'/usr/bin/chromedriver')
     wait.until(EC.element_to_be_clickable((By.ID,"ctl00_ContentPlaceHolder1_ddl_District")))
     return driver,wait
 
+# Track the loading spinner
+def still_loading(driver,wait):
+    try:
+        return driver.find_element_by_xpath('//*[@id="ctl00_ContentPlaceHolder1_upPgo"]').value_of_css_property("display")
+    except:
+        return "done"
+    
+    
 # Select Radio Buttons
 def select_hospital_type(driver,label):
     try:
         radio = driver.find_element_by_xpath("//*[@id='ctl00_ContentPlaceHolder1_rdo_Govt_Flag']/label[{}]".format(str(label)))
         radio.click()
+        driver.implicitly_wait(10)
     except:
         raise Exception("ERROR : Can't Select Hospital Type")
         
@@ -82,6 +91,8 @@ def select_hospital_type(driver,label):
 def select_district_and_wait_until_load(driver,wait,district_name):
     try:
         driver.find_element_by_xpath("//select[@name='ctl00$ContentPlaceHolder1$ddl_District']/option[text()='{}']".format(district_name)).click()
+        while still_loading(driver,wait) != "none":
+            driver.implicitly_wait(2)
         driver.implicitly_wait(10)
         wait.until(EC.element_to_be_clickable((By.ID,"ctl00_ContentPlaceHolder1_GridView2")))
     except:
@@ -92,8 +103,8 @@ def select_district_and_wait_until_load(driver,wait,district_name):
 def no_of_pages_for_selected_district_and_type(driver):
     try:
         pages=len(driver.find_elements_by_xpath("//*[@id='ctl00_ContentPlaceHolder1_GridView2']/tbody/tr[1]/td/table/tbody/tr/td"))
-        if pages==0:
-            pages =1
+        if pages == 0:
+            pages = 1
         return pages
     except:
         raise Exception("ERROR : Can't get number of pages for selected district and type")
@@ -102,8 +113,10 @@ def no_of_pages_for_selected_district_and_type(driver):
 def select_page_pagination_section(driver,page_no):
     try:
         driver.find_element_by_xpath("//*[@id='ctl00_ContentPlaceHolder1_GridView2']/tbody/tr[1]/td/table/tbody/tr/td[{}]/a".format(str(page_no))).click()
-        driver.implicitly_wait(10)
-
+        while still_loading(driver,wait) != "none":
+            driver.implicitly_wait(2)
+        driver.implicitly_wait(2)
+        wait.until(EC.element_to_be_clickable((By.ID,"ctl00_ContentPlaceHolder1_GridView2")))
     except:
         raise Exception("ERROR : Switching page for pagination")
         
@@ -112,6 +125,7 @@ def select_page_pagination_section(driver,page_no):
 def toggle_detailed_break_up_section(driver):
     try:
         driver.find_element_by_xpath("//*[contains(@id,'_div_card')]/div[2]/div[2]/div[1]/a").click()
+        driver.implicitly_wait(5)
     except:
         raise Exception("ERROR : Can't open detailed break up section")
 
@@ -147,22 +161,29 @@ def MobileCleanData(data):
     return len(result),result
 
 
+scraping  = False
+
 # Main Driver function for scraping
 def scrape_data(driver):
+    scraping = True
+
     # Get the list of hospotals entry
     entries = driver.find_elements_by_xpath("//*[contains(@id,'_div_card')]")
     
     if len(entries) == 0 :
         print("No items found")
+        scraping = False
         return
     
     toggle_detailed_break_up_section(driver)
 
+    
     # No of hospitals under selected category and dsitrct
     print(str(len(entries)) + " items fetched.")
 
     # Iterate through the entried and print the details
     for ei,e in enumerate(entries):
+        
     #     print(ei)
         print("Name : {}".format(e.find_element_by_xpath(".//h5").text))
         print("Address : {}".format(e.find_element_by_xpath("(.//div/div/div)[1]").text))
@@ -184,6 +205,15 @@ def scrape_data(driver):
             print("----category_ end-------\n")
             
         print("----------END------------")
+    scraping = False
+
+def still_loading(driver,wait):
+    try:
+        return driver.find_element_by_xpath('//*[@id="ctl00_ContentPlaceHolder1_upPgo"]').value_of_css_property("display")
+    except:
+        return "error"
+    
+
 
 driver,wait = init_scrape(chrome_options,endpoint)
 for district_lable,district_id in using_district_list.items():
@@ -191,15 +221,22 @@ for district_lable,district_id in using_district_list.items():
     for hospital_type in using_hospital_type:
         select_hospital_type(driver,using_hospital_type[hospital_type]["webkey"])
         select_district_and_wait_until_load(driver,wait,district_lable)
+        time.sleep(5)
         no_of_pages = no_of_pages_for_selected_district_and_type(driver)
-        
         global_buggy_pnum = 0
         for pg_no in range(no_of_pages):
             if pg_no != 0 or (pg_no==0 and global_buggy_pnum!=0) :
-                global_buggy_pnum = pg_no
-                select_page_pagination_section(driver,pg_no+1)
-                
+                while True:
+                    if scraping:
+                        time.sleep(2)
+                    else:
+                        global_buggy_pnum = pg_no
+                        select_page_pagination_section(driver,pg_no+1)
+                        time.sleep(10)
+                        break
+            
+            while still_loading(driver,wait) != "none":
+                driver.implicitly_wait(10)
             scrape_data(driver)
         print(f"Exiting from District : {district_lable} Type : {hospital_type} Page No : {pg_no}")
-
 
